cceaf74284cca1f3101f3b66575fcbffc236cd87
Vineet Gupta <vineet091@users.noreply.github.com>
2021-08-19 00:52:24 +0530

    sss

--- src/views/HomePage/HomePage.js
+++ src/views/HomePage/HomePage.js
@@ -33,51 +33,53 @@ function Home({ history, stockList, error, getStocksAPI, loading }) {
 
   return (
     <div className="home">
-      <h2 className="home-title">Stock Search</h2>
-      <div class="search-box">
-        <Autocomplete
-          freeSolo
-          loading={loading}
-          disableClearable
-          id="stock-search-input"
-          classes={{
-            inputRoot: "search-autocomplete",
-            input: "search-input",
-            root: "search-autocomplete-root"
-          }}
-          getOptionSelected={(option, value) =>
-            option.name === value["2. name"]
-          }
-          onChange={(event, newValue) => {
-            handleSearch(newValue);
-          }}
-          getOptionLabel={(option) => option["2. name"]}
-          options={stockList || []}
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              onChange={(ev) => {
-                // dont fire API if the user delete or not entered anything
-                if (ev.target.value !== "" || ev.target.value !== null) {
-                  setSearchText(ev.target.value);
-                  setErrorText("");
-                  this.debounceOnChange(ev.target.value);
-                }
-              }}
-              variant="outlined"
-              error={!!error || !!errorText}
-              helperText={error || errorText}
-            />
-          )}
-        />
-        <button
-          type="button"
-          id="btn-search"
-          class="btn-search"
-          onClick={() => validateSearch()}
-        >
-          Search
-        </button>
+      <div className="home-wrapper">
+        <h2 className="home-title">Stock Search</h2>
+        <div class="search-box">
+          <Autocomplete
+            freeSolo
+            loading={loading}
+            disableClearable
+            id="stock-search-input"
+            classes={{
+              inputRoot: "search-autocomplete",
+              input: "search-input",
+              root: "search-autocomplete-root"
+            }}
+            getOptionSelected={(option, value) =>
+              option.name === value["2. name"]
+            }
+            onChange={(event, newValue) => {
+              handleSearch(newValue);
+            }}
+            getOptionLabel={(option) => option["2. name"]}
+            options={stockList || []}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                onChange={(ev) => {
+                  // dont fire API if the user delete or not entered anything
+                  if (ev.target.value !== "" || ev.target.value !== null) {
+                    setSearchText(ev.target.value);
+                    setErrorText("");
+                    this.debounceOnChange(ev.target.value);
+                  }
+                }}
+                variant="outlined"
+                error={!!error || !!errorText}
+                helperText={error || errorText}
+              />
+            )}
+          />
+          <button
+            type="button"
+            id="btn-search"
+            class="btn-search"
+            onClick={() => validateSearch()}
+          >
+            Search
+          </button>
+        </div>
       </div>
     </div>
   );
