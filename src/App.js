32929063be88ee0b93495494fb5b82fbb2480123
Vineet Gupta <vineet091@users.noreply.github.com>
2021-08-19 13:59:30 +0530

    ddd

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import React, { PureComponent } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import HomePage from "./views/HomePage/HomePage";
 import NotFound from "./views/NotFound/NotFound";
-// import history from "./history";
 import StockDetailPage from "./views/StockDetailsPage/StockDetailsPage";
 export default class App extends PureComponent {
   render() {
     return (
       <div className="app-wrapper">
-        {/* <Loader />
-        <Message /> */}
         <BrowserRouter>
           <Switch>
             <Route exact path="/" component={HomePage} />
